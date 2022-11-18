7c395d5fa700989c57601be06dc30de6be6778f8
Akhilesh Chaurasiya <akhilesh.chaurasiya0598@gmail.com>
2022-11-18 13:06:19 +0530

    Adds second1

---
+++ ConsoleApp1/ConsoleApp1/Folder1/LocalTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Folder1
+{
+    public class LocalTime
+    {
+        public int Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Pressure2 { get; set; }
+    }
+}
